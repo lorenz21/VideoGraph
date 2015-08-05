6fc6f367c33cbf1b43cb6eac36bb8b2d368b5ba5
name <email>
2015-08-05 11:55:08 -1000

    set to launch with title screen

--- app/src/main/java/lcc/videograph/GraphingActivity.java
+++ app/src/main/java/lcc/videograph/GraphingActivity.java
@@ -2,6 +2,7 @@ package lcc.videograph;
 
 import android.app.Activity;
 import android.os.Bundle;
+
 public class GraphingActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
