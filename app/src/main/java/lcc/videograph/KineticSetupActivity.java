5dc317000cbd1f258cd38f93e7910e6633c45021
machinesxcix <steph.is.online@gmail.com>
2015-08-11 18:49:57 -1000

    kinetic friction guide

--- app/src/main/java/lcc/videograph/KineticSetupActivity.java
+++ app/src/main/java/lcc/videograph/KineticSetupActivity.java
@@ -1,37 +1,22 @@
 package lcc.videograph;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
 
-public class KineticMaterialsActivity extends AppCompatActivity {
+public class KineticSetupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_kinetic_materials);
-        this.next();
-    }
-
-    private void next() {
-        TextView nextTextView = (TextView)findViewById(R.id.textView_next);
-        nextTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent nextIntent = new Intent(KineticMaterialsActivity.this, KineticSetupActivity.class);
-                startActivity(nextIntent    );
-            }
-        });
+        setContentView(R.layout.activity_kinetic_setup);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_kinetic_materials, menu);
+        getMenuInflater().inflate(R.menu.menu_kinetic_setup, menu);
         return true;
     }
 
