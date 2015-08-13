1cee97be114e285f57fe9fd8c29e388b66464e60
name <email>
2015-08-12 14:19:21 -1000

    created a splash screen, programmed the new home screen

--- app/src/main/java/lcc/videograph/Home2Activity.java
+++ app/src/main/java/lcc/videograph/Home2Activity.java
@@ -1,38 +1,100 @@
 package lcc.videograph;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-
-public class Home2Activity extends ActionBarActivity {
-
+public class Home2Activity extends Activity {
+    String vidPathString;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home2);
+        this.captureButton();
+        this.analyzeButton();
+        this.guidesButton();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home2, menu);
-        return true;
+    private void captureButton() {
+        LinearLayout capture = (LinearLayout)findViewById(R.id.linearLayout_capture);
+        capture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentToRecordVideo = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                if (intentToRecordVideo.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(intentToRecordVideo, 1);
+                }
+            }
+        });
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private void analyzeButton(){
+        LinearLayout analyze = (LinearLayout)findViewById(R.id.linearLayout_analyze);
+        analyze.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                this.openGallery();
+            }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            private void openGallery() {
+                Intent intentOpenGallery = new Intent(Intent.ACTION_PICK, MediaStore.Video.Media.INTERNAL_CONTENT_URI);
+                startActivityForResult(intentOpenGallery, 1);
+            }
+        });
+    }
+
+    private void guidesButton() {
+        LinearLayout guides = (LinearLayout)findViewById(R.id.linearLayout_guides);
+        guides.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentGuides = new Intent(Home2Activity.this, Guides2Activity.class);
+                startActivity(intentGuides);
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            // When an Video is picked
+            if (requestCode == 1 && resultCode == RESULT_OK
+                    && null != data) {
+                // Get the Video from data
+                Uri selectedVideo = data.getData();
+                String[] filePathColumn = {MediaStore.Video.Media.DATA};
+
+                // Get the cursor
+                Cursor cursor = getContentResolver().query(selectedVideo,
+                        filePathColumn, null, null, null);
+                // Move to first row
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                // Used to get path of selected video.
+                vidPathString = cursor.getString(columnIndex);
+                cursor.close();
+                Intent i = new Intent(Home2Activity.this , ScaleVideo.class);
+                i.putExtra("vidPath", vidPathString);
+                startActivity(i);
+            }
+
         }
+        catch (Exception e) {
+            Toast.makeText(this, "There was an error", Toast.LENGTH_LONG)
+                    .show();
 
-        return super.onOptionsItemSelected(item);
+        }
     }
+
 }
+
